23548bf639e3daf3284c993b6f827b94d591e9f8
sunnyraj <mrsj5714@gmail.com>
2026-01-22 18:36:43 +0530

    autowire by name

---
+++ SpringExample/src/main/java/com/example/autowire/name/Car.java
@@ -0,0 +1,23 @@
+package com.example.autowire.name;
+
+public class Car {
+
+    private Specification specification;
+
+    public void setSpecification(Specification specification) {
+        this.specification = specification;
+    }
+
+    public void displayDetails(){
+        System.out.println("Car Details: " + specification.toString());
+    }
+//    private Specification specification1;
+//
+//    public void setSpecification1(Specification specification) {
+//        this.specification1 = specification;
+//    }
+//
+//    public void displayDetails(){
+//        System.out.println("Car Details: " + specification1.toString());
+//    }
+}
