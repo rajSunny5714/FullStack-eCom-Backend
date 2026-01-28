f5eeaa3f5ab1e435866bcd6459f1591f1d475a57
sunnyraj <mrsj5714@gmail.com>
2026-01-28 19:49:31 +0530

    files updated

--- FirstSpring/src/main/java/com/embarkX/FirstSpring/HelloResponse.java
+++ FirstSpring/src/main/java/com/embarkX/FirstSpring/HelloResponse.java
@@ -1,2 +1,17 @@
-package com.embarkX.FirstSpring;public class HelloResponse {
+package com.embarkX.FirstSpring;
+
+public class HelloResponse {
+    private String message;
+
+    public HelloResponse(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
